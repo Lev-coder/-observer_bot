7d19194e090aeb3c9775a5f3718f7ff131b35b08
Lev-coder <54110434+Lev-coder@users.noreply.github.com>
2021-08-03 18:24:02 +0300

    add class UsersResources definition

---
+++  database/ tables/UsersResurces.py
@@ -0,0 +1,19 @@
+import ITable
+
+class UsersResources(ITable):
+
+    def __init__(self):
+        self._tableName = "users_resources"
+
+    def up(self):
+        return f"""
+        CREATE TABLE IF NOT EXISTS {self._tableName}
+        (
+             chat_id int,
+             resource_id int
+        )"""
+
+    def down(self):
+        return f"""
+        DROP TABLE {self._tableName}
+        """
