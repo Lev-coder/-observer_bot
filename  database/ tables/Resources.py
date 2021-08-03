069052c6437f3f03ac2d1e826908d53e6b9f9e63
Lev-coder <54110434+Lev-coder@users.noreply.github.com>
2021-08-03 18:20:02 +0300

    add class Resurces definition

---
+++  database/ tables/Resources.py
@@ -0,0 +1,19 @@
+import ITable
+
+class Resurces (ITable):
+
+    def __init__(self):
+        self._tableName = "resources"
+
+    def up(self):
+        return f"""
+        CREATE TABLE IF NOT EXISTS {self._tableName}
+        (
+             name text,
+             link text,
+         )"""
+
+    def down(self):
+        return f"""
+        DROP TABLE {self._tableName}
+        """
